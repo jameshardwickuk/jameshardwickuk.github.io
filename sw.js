29cf39ad2d09edfcf21ed5a1570daeef9c80fe70
jameshardwickuk <james.hardwick@hotmail.co.uk>
2026-09-03 09:37:09 +0100

    Add Bowser service worker

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "moorhead-fuel-v3";
+const ASSETS = ["./index.html", "./assets/app.css", "./assets/app.js", "./assets/docs-catalog.js", "./assets/fleet-catalog.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
